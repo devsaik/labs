daa7c27161e16f61e5d90f41152a12dc5d79f655
devsaik <emailsaik@gmail.com>
2015-10-17 20:31:32 -0700

    2 more changes

---
+++ src/app/main/main.controller.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('labs')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController($timeout,$scope,ivhTreeviewMgr,ivhTreeviewBfs,TreeServiceUtils) {
+
+  }
+})();
